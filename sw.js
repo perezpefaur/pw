fb5193d3f24d17209fefedfd95711fa4ec5aa008
perezpefaur <perezpefaur@uc.cl>
2022-04-28 22:20:27 -0400

    fixing reciving notification

--- sw.js
+++ sw.js
@@ -14,6 +14,23 @@ self.addEventListener("fetch", e => {
         )
 });
 
-self.addEventListener("push", () => {
-    self.registration.sendNotification('test mesage', {}) // se puede poner iconos
-});
+self.addEventListener('push', function(e) {
+    var options = {
+      body: 'This notification was generated from a push!',
+      icon: 'images/logo192.png',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: '2'
+      },
+      actions: [
+        {action: 'explore', title: 'Explore this new world',
+          icon: 'images/logo192.png'},
+        {action: 'close', title: 'Close',
+          icon: 'images/logo192.png'},
+      ]
+    };
+    e.waitUntil(
+      self.registration.showNotification('Hello world!', options)
+    );
+  });
