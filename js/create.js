06251dbd9819b40cc407b4365d566a4277c2d7a9
perezpefaur <perezpefaur@uc.cl>
2022-04-29 16:43:47 -0400

    mode cors

--- js/create.js
+++ js/create.js
@@ -9,6 +9,7 @@ function create() {
     console.log(guest_can_pause.checked);
     fetch('http://127.0.0.1:8000/api/create-room', { 
             method: 'POST',
+            mode: 'cors',
             body: JSON.stringify({
                 votes_to_skip: votes_to_skip.value,
                 guest_can_pause: guest_can_pause.checked
