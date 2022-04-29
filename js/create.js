34db5d68986cb06de97b614199e700d946a41c09
perezpefaur <perezpefaur@uc.cl>
2022-04-29 14:57:38 -0400

    check

--- js/create.js
+++ js/create.js
@@ -1,6 +1,10 @@
 function create() {
     let votes_to_skip = document.getElementById('votes_to_skip');
     let guest_can_pause = document.getElementById('guest_can_pause');
+    let errorElement = document.getElementById('error');
+    if (votes_to_skip.value == '' || votes_to_skip.value == null){
+        errorElement.innerText = 'Tienes que ingresar un numero valido.'
+    }
     console.log(votes_to_skip.value);
     console.log(guest_can_pause.checked);
 }
