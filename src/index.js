3d38bdc075ed1d8de105ec7565e0094679256371
perezpefaur <perezpefaur@uc.cl>
2022-04-29 14:09:35 -0400

    ver si con esto salvo

--- src/index.js
+++ src/index.js
@@ -5,13 +5,3 @@ if ("serviceWorker" in navigator) {
         console.error(error);
     });
 }
-
-const votes_to_skip = document.getElementsById('votes_to_skip')
-const guest_can_pause = document.getElementsById('guest_can_pause')
-const form = document.getElementsById('form')
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-})
-
-console.log(votes_to_skip);
