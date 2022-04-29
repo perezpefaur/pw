d90c6be50571e460fb4f33d5e8b17b0315be5006
perezpefaur <perezpefaur@uc.cl>
2022-04-29 14:04:08 -0400

    ver

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ if ("serviceWorker" in navigator) {
         console.error(error);
     });
 }
+
+const votes_to_skip = document.getElementsById('votes_to_skip')
+const guest_can_pause = document.getElementsById('guest_can_pause')
+const form = document.getElementsById('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
