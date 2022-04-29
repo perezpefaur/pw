50e521c59e04bd37cea9af3396a86319d00d38dd
perezpefaur <perezpefaur@uc.cl>
2022-04-29 14:48:45 -0400

    solve

--- js/create.js
+++ js/create.js
@@ -1,14 +1,11 @@
 function create() {
-    let votes_to_skip = document.getElementsById('votes_to_skip');
-    let guest_can_pause = document.getElementsById('guest_can_pause');
-    let form = document.getElementsById('form');
+    let votes_to_skip = document.getElementById('votes_to_skip');
+    let guest_can_pause = document.getElementById('guest_can_pause');
+    let form = document.getElementById('form');
     console.log(votes_to_skip.value);
     console.log(guest_can_pause.value);
 }
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-})
 console.log('matias');
 console.log(votes_to_skip);
 console.log(guest_can_pause);
