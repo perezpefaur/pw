93eb6c0b4dcd6f6bb24b42e125bc6005c0b2c511
perezpefaur <perezpefaur@uc.cl>
2022-04-29 14:54:51 -0400

    only recive number

--- js/create.js
+++ js/create.js
@@ -1,9 +1,7 @@
 function create() {
     let votes_to_skip = document.getElementById('votes_to_skip');
     let guest_can_pause = document.getElementById('guest_can_pause');
-    let form = document.getElementById('form');
     console.log(votes_to_skip.value);
-    console.log(guest_can_pause.value);
     console.log(guest_can_pause.checked);
 }
 
