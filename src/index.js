dae9faa81fe0615b14a601ee9b6f6a05fc842acc
perezpefaur <perezpefaur@uc.cl>
2022-04-28 21:58:47 -0400

    error handle

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 if ("serviceWorker" in navigator) {
-    let sw = await navigator.serviceWorker.register("sw.js").then(registration => {
+    navigator.serviceWorker.register("sw.js").then(registration => {
         console.log(registration);
+        async function subscribe() {
+            let sw = await navigator.serviceWorker.ready;
+            let push = await sw.pushManager.subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: 'BB2VclKxEI2khPyc0DZyhtlvhBYwfGKr-cd-PSk5-0h2xRL6vXNYF8kSXJe5s7L0lR2-YuBpVppGUYz4pEmsTeo'
+            })
+            console.log(JSON.stringify(push)) // se supone que se deberia volver a la api
+        }
     }).catch(error => {
         console.error(error);
     });
-
-    async function subscribe() {
-        let sw = await navigator.serviceWorker.ready;
-        let push = await sw.pushManager.subscribe({
-            userVisibleOnly: true,
-            applicationServerKey: 'BB2VclKxEI2khPyc0DZyhtlvhBYwfGKr-cd-PSk5-0h2xRL6vXNYF8kSXJe5s7L0lR2-YuBpVppGUYz4pEmsTeo'
-        })
-        console.log(JSON.stringify(push)) // se supone que se deberia volver a la api
-    }
 }
