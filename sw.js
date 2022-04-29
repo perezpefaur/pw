d77479859a6d85b15042801a7fe8a1b0f3bc2fcf
perezpefaur <perezpefaur@uc.cl>
2022-04-28 22:09:23 -0400

    Error fix

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ self.addEventListener("install", e => {
 
 self.addEventListener("fetch", e => {
     e.respondWith(
-        cache.match(e.request).then(response => {
+        caches.match(e.request).then(response => {
             return response || fetch(e.request);
         })
         )
