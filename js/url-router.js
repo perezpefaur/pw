c411e3437e99de3d16d760c683b7212f38078617
perezpefaur <perezpefaur@uc.cl>
2022-04-30 01:03:45 -0400

    ver

--- js/url-router.js
+++ js/url-router.js
@@ -64,7 +64,7 @@ const urlLocationHandler = async () => {
 
 fetch('http://127.0.0.1:8000/api/user-in-room', { 
     method: 'GET',
-    credentials: 'include',
+    credentials: 'same-origin',
     headers: {
         "accept": "application/json",
         'Content-Type': 'application/json'
