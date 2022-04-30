77d579eedad326830cf534c576482fdcc38f9285
perezpefaur <perezpefaur@uc.cl>
2022-04-30 10:43:00 -0400

    ver leave

--- js/create.js
+++ js/create.js
@@ -28,6 +28,11 @@ function create() {
             response.text().then((data) => {
                 document.getElementById("content").innerHTML = data;
                 document.getElementById("code").innerHTML = json['code'];
+                if (json['guest_can_pause'] == false || json['guest_can_pause'] == 'false') {
+                    document.getElementById("icon_bool").innerHTML = "<img class=\"icono\" src=\"https://cdn-icons-png.flaticon.com/512/1810/1810746.png\" style=\"width: 60px;\" >";
+                } else {
+                    document.getElementById("icon_bool").innerHTML = "<img class=\"icono\" src=\"https://cdn-icons-png.flaticon.com/512/6276/6276686.png\" style=\"width: 60px;\" >";
+                }
             });
         });
         }).catch(err => alert(err))
@@ -63,6 +68,24 @@ function join() {
                 } else {
                     document.getElementById("icon_bool").innerHTML = "<img class=\"icono\" src=\"https://cdn-icons-png.flaticon.com/512/6276/6276686.png\" style=\"width: 60px;\" >";
                 }
+            });
+        });
+        }).catch(err => alert(err))
+}
+
+function exit() {
+    fetch('http://127.0.0.1:8000/api/leave-room', { 
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                "accept": "application/json",
+                'Content-Type': 'application/json'
+            }
+            }).then(response => response.json())
+      .then(json => {
+          fetch('/').then((response) => {
+            response.text().then((data) => {
+                document.getElementById("content").innerHTML = data;
             });
         });
         }).catch(err => alert(err))
